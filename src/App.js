d274c4e3d2c978c3289da6161a092cd83039be69
chathumikethmini <kethminichathumi@gmail.com>
2025-02-26 11:25:53 +0530

    splash screen

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import SplashScreen from  "./component/Splash1/SplashScreen"; // Import the SplashScreen component
+// import './App.css'; // Optional: import other styles for your main app
+// import Home from './component/home/home.js';
+import SplashScreen from "./component/test/SplashScreen";
+
+function App() {
+  return (
+    // <SplashScreen/>
+    <div>
+      
+      <SplashScreen/>
+      {/* <Home/> */}
+    
+    </div>
+    
+    // <div>
+    //   <SplashScreen/> {/* Include the SplashScreen */}
+    //   {/* <div className="app-content"> */}
+    //     {/* Your main app content goes here */}
+    //     {/* <h1>Welcome to My App!</h1>
+    //     <p>This is the content that appears after the splash screen.</p>
+    //   </div> */}
+    // </div>
+  );
+}
+
+export default App;
+
